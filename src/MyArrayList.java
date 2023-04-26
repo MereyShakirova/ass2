835f0823f45e505de3f5bb27eaa546bcf2e91ff3
Shakirova Merei <sh.merei05@gmail.com>
2023-04-26 17:00:09 +0600

    commit5

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MyArrayList<T> implements  MyList {
     private T[] list;
     private int size;
@@ -22,11 +25,14 @@ public class MyArrayList<T> implements  MyList {
         }
         return false;
     }
-    public  void add(Object item){
-        if(size==arraySize){
+    public  void add(Object item, int index){
+        if(size == arraySize){
             resize();
         }
-        list[size++] = (T) item;
+        for (int i = size; i > index; i--){
+            list[i] = list[i-1];
+        }
+        list[index] = (T) item;
         size++;
     }
     public boolean remove(Object item){
@@ -59,8 +65,31 @@ public class MyArrayList<T> implements  MyList {
         }
         return -1;
     }
+    public int lastIndexOf(Object o) {
+        int index = -1;
+        if (o == null) return -1;
+        for (int i = 0; i < size; i++) {
+            if (o.equals(list[i])) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public void sort(){
+        Arrays.sort(list, 0, size);}
 
-
+    public <T extends Comparable<T>> int compare(T a, T o){return a.compareTo(o);}
+    private void resize(){
+        arraySize *= 2;
+        Object[] increasedList = new Object[arraySize];
+        for (int i = 0; i < size; i++){
+            increasedList[i] = list[i];
+        }
+        list = (T[]) increasedList;
+    }
+    public String toString() { return Arrays.toString(Arrays.copyOf(list, size));}
 }
+
+
 
 
