3adc64ff5a99c2e52aee1991d8d328d3f998c04a
Shakirova Merei <sh.merei05@gmail.com>
2023-04-26 20:16:51 +0600

    commit9

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -20,7 +20,33 @@ public class MyLinkedList<T extends  Comparable<T>> implements MyList {
         tail = newNode;
         size = 1;
     }
+    @Override
     public int size(){return size;}
+    @Override
+    public boolean contains(Object o) {
+        Node<T> current = head;
+        while (current != null) {
+            if(current.value.equals(o)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+    @Override
+    public void add(Object item) {
+        Node<T> newNode = new Node<>((T) item);
+        If (head == null){
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
 
 
 
