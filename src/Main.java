9d1e4bc3ddfa4e93d9783bdd1f253f7d89da50a4
Shakirova Merei <sh.merei05@gmail.com>
2023-04-26 15:30:04 +0600

    commit3

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.Node;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(45);
+        myArrayList.add(46);
+        myArrayList.add(47);
+        myArrayList.add(48);
+        myArrayList.add(49);
+        myArrayList.add(50);
+        myArrayList.add(31);
+        myArrayList.remove(6);
+
+    }
+}
