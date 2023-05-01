dd746d8b6f2da67134aeb4580017b23039aa9d9f
Shakirova Merei <sh.merei05@gmail.com>
2023-05-02 02:11:26 +0600

    assignment3-34

--- src/MyArrayListQueque.java
+++ src/MyArrayListQueque.java
@@ -1,30 +1,30 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 public class MyArrayListQueque<T> {
-    private ArrayList<T> queue;
+    private ArrayList<T> queue;// returns front element of queue without removing it
     public MyArrayListQueque() {
-        queue = new ArrayList<>();
+        queue = new ArrayList<>();//constructor initializes instance variable
     }
     public void enqueue(T element){
-        queue.add(element);
+        queue.add(element);//adds item to back of the queue
     }
     public T dequeue(){
         if(queue.isEmpty()){
-            throw new NoSuchElementException();
+            throw new NoSuchElementException();//throws exception if queue is empty
         }
-        return queue.remove(0);
+        return queue.remove(0);//removes and returns front element of queue
     }
     public T peek() {
         if(queue.isEmpty()){
-            throw new NoSuchElementException();
+            throw new NoSuchElementException();// throws exception if queue is empty
         }
-        return queue.get(0);
+        return queue.get(0);//returns front element of queue without removing it
     }
     public boolean isEmpty(){
-        return queue.isEmpty();
+        return queue.isEmpty();//returns true if queue is empty, false otherwise
     }
     public int size(){
-        return queue.size();
+        return queue.size();//returns number of elements in queue
     }
 
 }
--- src/MyLinkedListQueque.java
+++ src/MyLinkedListQueque.java
@@ -1,29 +1,30 @@
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 public class MyLinkedListQueque<T> {
-    private LinkedList<T> queue;
-    public MyLinkedListQueque() {
+    private LinkedList<T> queue;//instance variable of type MyLinkedList
+    public MyLinkedListQueque() {// constructor initializes instance variable
         queue = new LinkedList<>();
     }
     public void enqueue(T element){
+        // adds item to back of the queue
         queue.addLast(element);
     }
     public T dequeue() {
         if(queue.isEmpty()){
-            throw new NoSuchElementException();
+            throw new NoSuchElementException();//throws exception if queue is empty
         }
-        return queue.removeFirst();
+        return queue.removeFirst();// removes and returns front element of queue
     }
     public T peek(){
         if(queue.isEmpty()){
-            throw new NoSuchElementException();
+            throw new NoSuchElementException();// throws exception if queue is empty
         }
-        return queue.getFirst();
+        return queue.getFirst();// returns front element of queue without removing it
     }
     public boolean isEmpty(){
-        return queue.isEmpty();
+        return queue.isEmpty();// returns front element of queue without removing it
     }
     public int size(){
-        return queue.size();
+        return queue.size();// returns front element of queue without removing it
     }
 }
