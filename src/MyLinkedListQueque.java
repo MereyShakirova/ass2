1bb5ab0f661f1cfbac85594d2b7b9381e1160062
Shakirova Merei <sh.merei05@gmail.com>
2023-05-01 22:55:31 +0600

    assignment3-13

--- src/MyLinkedListQueque.java
+++ src/MyLinkedListQueque.java
@@ -1,2 +1,8 @@
-public class MyLinkedListQueque {
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+public class MyLinkedListQueque<T> {
+    private LinkedList<T> queque;
+    public MyLinkedListQueque() {
+        queque = new LinkedList<>();
+    }
 }
