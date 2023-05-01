70b8351370c43f01b81cb6bbf10b64aa835fc77e
Shakirova Merei <sh.merei05@gmail.com>
2023-05-01 20:15:17 +0600

    assignment3-7

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -19,7 +19,10 @@ public class MyArrayListStack<T> {
         if(stack.isEmpty()){
             throw new EmptyStackException();
         }
-        return stack.remove(stack.size()+1);
+        return stack.get(stack.size()-1);
+    }
+    public boolean isEmpty(){
+        return stack.isEmpty();
     }
 
 }
