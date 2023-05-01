0c12de10eef7f7c1146719e0ddecf4a004b283b9
Shakirova Merei <sh.merei05@gmail.com>
2023-05-01 20:05:48 +0600

    assignment3-4

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
+    private ArrayList<T> stack;
+    public MyArrayListStack() {
+        stack =new ArrayList<>();
+    }
+
 
 }
