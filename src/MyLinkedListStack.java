d356ed82aeb654553d90b029e78964994d6852c9
Shakirova Merei <sh.merei05@gmail.com>
2023-05-01 18:52:42 +0600

    assignment3

---
+++ src/MyLinkedListStack.java
@@ -0,0 +1,18 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+public class MyLinkedListStack<T> {
+    private LinkedList<T> list;
+    public MyLinkedListStack(){
+        list = new LinkedList<>();
+    }
+    public void push(T element) {
+        list.addFirst(element);
+    }
+    public T pop(){
+        if(list.isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.removeFirst();
+    }
+
+}
