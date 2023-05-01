418d4cc5aa00835ef0d7bac18af7ea12911269bc
Shakirova Merei <sh.merei05@gmail.com>
2023-05-01 22:38:16 +0600

    assignment3-11

--- src/MyArrayListQueque.java
+++ src/MyArrayListQueque.java
@@ -8,5 +8,12 @@ public class MyArrayListQueque<T> {
     public void enqueque(T element){
         queque.add(element);
     }
+    public T dequeque(){
+        if(queque.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return queque.remove(0);
+    }
+
 
 }
